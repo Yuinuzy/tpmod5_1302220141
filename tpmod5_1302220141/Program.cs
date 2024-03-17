556dac96bf0023b520198a403a2998fd6c9852a1
Yuinuzy <tsaqif@student.telkomuniversity.ac.id>
2024-03-17 22:08:26 +0700

    menambahkan class DataGeneric

---
+++ tpmod5_1302220141/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tpmod5_1302220141;
+class program
+{
+    public static void Main(string[] args)
+    {
+        halogeneric<String> data = new halogeneric<string>("1302220141");
+        data.PrintData();
+    }
+}
---
+++ tpmod5_1302220141/halogeneric.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmod5_1302220141
+{
+
+    internal class halogeneric<T>
+    {
+        private T data;
+        public halogeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Hello " +  data);
+        }
+    }
+}
